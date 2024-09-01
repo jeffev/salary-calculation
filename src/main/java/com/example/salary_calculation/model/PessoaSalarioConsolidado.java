635648c51236430969887a5298dac2dcdd55a758
DESKTOP-TP0PGUH\jeffe <jeffev123@gmail.com>
2024-09-01 10:29:23 -0300

    retirado imports desnecessarios

--- src/main/java/com/example/salary_calculation/model/PessoaSalarioConsolidado.java
+++ src/main/java/com/example/salary_calculation/model/PessoaSalarioConsolidado.java
@@ -2,8 +2,6 @@ package com.example.salary_calculation.model;
 
 import jakarta.persistence.Version;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
